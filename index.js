cc5e96be660cadb5bbe3f1f945a5ec113e45bc70
Abby Nyakara <abbynyakara@gmail.com>
2022-05-16 12:29:56 +0300

    correct linter errors

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const openMenu = document.querySelector(".menu-icon button");
-const closeMenu = document.querySelector(".mobile-menu button");
+const openMenu = document.querySelector('.menu-icon button');
+const closeMenu = document.querySelector('.mobile-menu button');
 
-openMenu.addEventListener("click", ()=>{
-    document.querySelector(".mobile-menu").classList.add("show");
+openMenu.addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.add('show');
 })
 
-closeMenu.addEventListener("click", ()=>{
-    document.querySelector(".mobile-menu").classList.remove("show");
-})
-
+closeMenu.addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.remove('show');
+});
