f995cc9aa29095ee75007124039b0450bfc4078e
Abby Nyakara <abbynyakara@gmail.com>
2022-05-16 11:36:50 +0300

    mobile-menu

---
+++ index.js
@@ -0,0 +1,11 @@
+const openMenu = document.querySelector(".menu-icon button");
+const closeMenu = document.querySelector(".mobile-menu button");
+
+openMenu.addEventListener("click", ()=>{
+    document.querySelector(".mobile-menu").classList.add("show");
+})
+
+closeMenu.addEventListener("click", ()=>{
+    document.querySelector(".mobile-menu").classList.remove("show");
+})
+
