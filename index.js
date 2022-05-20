d0124c531f2cfd0b229b178b7a6045d1426b00a6
Abby Nyakara <abbynyakara@gmail.com>
2022-05-20 17:53:15 +0300

    create popup

--- index.js
+++ index.js
@@ -28,3 +28,94 @@ form.addEventListener('submit', (e) => {
     errorMsg.classList.add('display-content');
   }
 });
+
+// Popup window 
+
+const portfolio = document.querySelector('#portfolio');
+
+const projectArray = [
+  {
+    mobileImg: 'images/tonic-section.svg',
+    desktopImg: 'images/desktop-snap1.png',
+    cardTitle: 'Tonic',
+    cardDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    class:'card',
+  },
+  
+  {
+    mobileImg: 'images/availability-section.svg',
+    desktopImg: 'images/desktop-snap2.png',
+    cardTitle: 'Multi-Post Stories',
+    cardDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    class: 'card-2'
+  },
+
+  {
+    mobileImg: 'images/tonic-section.svg',
+    desktopImg: 'images/desktop-snap1.png',
+    cardTitle: 'Tonic',
+    cardDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    class: 'card'
+  },
+
+  {
+    mobileImg: 'images/tonic-section.svg',
+    desktopImg: 'images/desktop-snap1.png',
+    cardTitle: 'Tonic',
+    cardDesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    class: 'card-2'
+  }
+]
+
+portfolio.innerHTML = projectArray.map((project) => {
+  return html = `
+  <section class="card ${project.class} work">
+  <img class= "mobile-img" src="${project.mobileImg}" alt="" aria-hidden="true">
+  <img class= "desktop-img" src="${project.desktopImg}" alt="" aria-hidden="true">
+
+  <div class="content">
+      <h2>${project.cardTitle}</h2>
+      <div class="experience-section">
+          <p class="canopy">Canopy</p>
+          <img src="images/Counter.svg" alt="" aria-hidden="true">
+          <p class="bullet">Back End Dev</p>
+          <img src="images/Counter.svg" alt="" aria-hidden="true">
+          <p class="bullet">2015</p>
+      </div>
+      <p class="card-text">${project.cardDesc}</p>
+      <ul class="skills">
+          <li>html</li>
+          <li>css</li>
+          <li>javaScript</li>
+      </ul>
+      <button class="submit" type="submit">See Project</button>
+  </div>
+</section>
+  `
+});
+
+
+const submitButton = document.querySelectorAll('.submit');
+const popup = document.querySelector('.popup');
+const cancel = document.querySelector('.cancel');
+const mainContent = document.querySelector('.page-content');
+
+submitButton.forEach(button =>{
+  button.addEventListener('click', ()=>{
+    popup.classList.remove('hide');
+    mainContent.classList.add('blur');
+  });
+});
+
+cancel.addEventListener('click', ()=>{
+  popup.classList.add('hide');
+  mainContent.classList.remove('blur');
+});
+
+
+
+
+
+
+
+
